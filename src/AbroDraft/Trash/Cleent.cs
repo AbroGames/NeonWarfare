d74e90307aefc800eac42d7266488951eb5807aa
Bronuh <bronyx@yandex.ru>
2024-01-28 21:39:16 +0300

    Add broken network example

---
+++ src/AbroDraft/Trash/Cleent.cs
@@ -0,0 +1,28 @@
+﻿using Godot;
+using System;
+using AbroDraft.Net;
+using AbroDraft.Net.Packets;
+
+public partial class Cleent : Button
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Pressed += () =>
+		{
+			Network.ConnectToRemoteServer("localhost", 8800);
+			Network.Api.ConnectedToServer += Greeting;
+		};
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private static void Greeting()
+	{
+		Network.Api.ConnectedToServer -= Greeting;
+		Network.SendPacket(new HelloPacket());
+	}
+}
---
+++ src/AbroDraft/Trash/Servir.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+using System;
+using AbroDraft.Net;
+
+public partial class Servir : Button
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Pressed += () =>
+		{
+			Network.CreateServer(8800);
+		};
+	}
+}
