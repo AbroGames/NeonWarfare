992533104b83c5130ff8adfdd6d41c5c48466e20
Bronuh <bronyx@yandex.ru>
2024-01-28 10:34:04 +0300

    Add basic input

---
+++ src/AbroDraft/Keys.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace MicroSurvivors;
+
+public static class Keys
+{
+    public static readonly StringName Up = "KeyUp";
+    public static readonly StringName Down = "KeyDown";
+    public static readonly StringName Left = "KeyLeft";
+    public static readonly StringName Right = "KeyRight";
+}
