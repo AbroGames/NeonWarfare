f8528976eadeeaedc9d8c561a118738b336d0dde
KeyJ148 <kejik148@gmail.com>
2024-01-28 18:07:32 +0300

    Add Character

--- src/AbroDraft/WorldEntities/Character.cs
+++ src/AbroDraft/WorldEntities/Character.cs
@@ -7,6 +7,10 @@ namespace AbroDraft;
 
 public partial class Character : Node2D
 {
+	
+	[Export] private double _movementSpeed = 250; // in pixels/sec
+	[Export] private double _rotationSpeed = 300; // in degree/sec
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -16,6 +20,48 @@ public partial class Character : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		RotateToMouse(delta);
+		Move(delta);
+	}
+
+	private void Move(double delta)
+	{
+		var movementInput = GetInput();
+		Position += movementInput * _movementSpeed * delta;
+	}
+	
+	private Vector2 GetInput()
+	{
+		return Input.GetVector(Keys.Left, Keys.Right, Keys.Up, Keys.Down);
+	}
+
+	private void RotateToMouse(double delta)
+	{
+		//Куда хоти повернуться
+		double targetAngle = GetAngleToMouse();
+		//На какой угол надо повернуться (знак указывает направление)
+		double deltaAngleToTargetAngel = Mathf.AngleDifference(Rotation - Mathf.Pi / 2, targetAngle);
+		//Только направление (-1, 0, 1)
+		double directionToTargetAngel = Mathf.Sign(deltaAngleToTargetAngel);
+		//Максимальная скорость поворота (за секунду)
+		double rotationSpeedRad = Mathf.DegToRad(_rotationSpeed);
+		//Максимальная скорость поворота (за прошедшее время)
+		rotationSpeedRad *= delta;
+		//Если надо повернуться на угол меньший максимальной скорости, то обрезаем скорость, чтобы повернуться ровно в цель
+		rotationSpeedRad = Math.Min(rotationSpeedRad, Math.Abs(deltaAngleToTargetAngel));
+		//Добавляем к скорости поворота направление, чтобы поворачивать в сторону цели
+		rotationSpeedRad *= directionToTargetAngel;
+		//Поворачиваемся на угол
+		Rotation += rotationSpeedRad;
+	}
+	
+	private double GetAngleToMouse()
+	{
+		// Получаем текущую позицию мыши
+		var mousePos = GetGlobalMousePosition();
+		// Вычисляем вектор направления от объекта к мыши
+		var mouseDir = GlobalPosition.DirectionTo(mousePos);
+		// Вычисляем направление от объекта к мыши
+		return mouseDir.Angle();
 	}
 }
