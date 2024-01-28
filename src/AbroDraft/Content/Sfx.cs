d468f49335ef0f2d045b96ff2be26295755c12fa
Bronuh <bronyx@yandex.ru>
2024-01-28 11:09:06 +0300

    Add asset references

---
+++ src/AbroDraft/Content/Sfx.cs
@@ -0,0 +1,213 @@
+﻿using KludgeBox.Collections;
+
+namespace Game.Content
+{
+	public static class Sfx
+	{
+		public const string SoundsDir = "res://Assets/Audio/Sounds";
+		
+		// impacts
+		public static RandomPicker<string> Hit { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/impact/hit{{0}}.wav".BatchNumber(1, 3)
+		);
+
+		public static RandomPicker<string> FuturisticHit { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/impact/hit_futuristic{{0}}.wav".BatchNumber(1, 4)
+		);
+
+		public static RandomPicker<string> Zap { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/impact/zap_impact{{0}}.wav".BatchNumber(1, 3)
+		);
+
+		public static RandomPicker<string> GlitchyImpact { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/impact/glitchy_impact{{0}}.wav".BatchNumber(1, 5)
+		);
+
+		public static RandomPicker<string> HornImpact { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/impact/horn_impact{{0}}.wav".BatchNumber(1, 3)
+		);
+
+		public static RandomPicker<string> DeepImpack { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/impact/deep_impact{{0}}.wav".BatchNumber(1, 4)
+		);
+
+		
+		
+
+		// bass
+		public static RandomPicker<string> BassDrop { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/bass/bass_drop{{0}}.wav".BatchNumber(1, 2)
+		);
+
+		public static RandomPicker<string> BassDowner { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/bass/bass_downer1.wav"
+		);
+
+		public static RandomPicker<string> Bass { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/bass/bass1.wav"
+		);
+
+		
+		
+
+		// magic
+		public static RandomPicker<string> FireBeam { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/magic/fire_beam{{0}}.wav".BatchNumber(1, 3)
+		);
+
+		public static RandomPicker<string> FireIgnite { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/magic/fire_ignite{{0}}.wav".BatchNumber(1, 2)
+		);
+
+		public static RandomPicker<string> FireWhoosh { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/magic/fire_whoosh{{0}}.wav".BatchNumber(1, 3)
+		);
+
+		public static RandomPicker<string> Spell { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/magic/spell1.wav"
+		);
+
+		public static RandomPicker<string> ThunderClap { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/magic/thunder_close{{0}}.wav".BatchNumber(1, 4)
+		);
+
+		public static RandomPicker<string> TurbulentFire { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/magic/turbulent_fire.wav"
+		);
+
+		public static RandomPicker<string> WindGust { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/magic/wind_gust.wav"
+		);
+
+		public static RandomPicker<string> WooshShort { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/magic/woosh_short{{0}}.wav".BatchNumber(1, 4)
+		);
+
+
+		
+		
+		// misc
+		public static RandomPicker<string> Bubbles { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/misc/bubble{{0}}.wav".BatchNumber(1, 4)
+		);
+
+		public static RandomPicker<string> Arc { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/misc/arc.ogg"
+		);
+
+		public static RandomPicker<string> Buzz { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/misc/buzz.wav"
+		);
+
+		public static RandomPicker<string> ChestOpen { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/misc/chest_open.wav"
+		);
+
+		public static RandomPicker<string> ChestClose { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/misc/chest_close.wav"
+		);
+
+		public static RandomPicker<string> FireworkLaunch { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/misc/firework_launch{{0}}.wav".BatchNumber(1, 4)
+		);
+
+		public static RandomPicker<string> Grab { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/misc/grab.wav"
+		);
+
+		public static RandomPicker<string> Pop { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/misc/pop.ogg"
+		);
+
+		public static RandomPicker<string> Shrill { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/misc/shrill.wav"
+		);
+
+		public static RandomPicker<string> Throw { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/misc/throw{{0}}.wav".BatchNumber(1, 5)
+		);
+
+		
+		
+		// weapons
+		public static RandomPicker<string> ElectronicShot { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/electronic_shot{{0}}.wav".BatchNumber(1, 3)
+		);
+
+		public static RandomPicker<string> HandgunShot { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/handgun_shot{{0}}.wav".BatchNumber(1, 2)
+		);
+
+		public static RandomPicker<string> HandgunShotAlt { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/handgun_shot.wav"
+		);
+
+		public static RandomPicker<string> GrenadelauncherShot { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/grenade_launcher_launch.wav"
+		);
+
+		public static RandomPicker<string> HandgunReload { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/handgun_reload.wav"
+		);
+
+		public static RandomPicker<string> HeavyRifleShot { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/heavy_rifle_shot.ogg"
+		);
+
+		public static RandomPicker<string> LaserShot { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/laser_shot.wav"
+		);
+
+		public static RandomPicker<string> LaserShots { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/laser_shots.wav"
+		);
+
+		public static RandomPicker<string> LaserShotShort { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/laser_shot_short.wav"
+		);
+
+		public static RandomPicker<string> HeavyLauncher { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/launcher_1.wav"
+		);
+
+		public static RandomPicker<string> PlasmaShot { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/plasma_shot{{0}}.wav".BatchNumber(1, 4)
+		);
+
+		public static RandomPicker<string> RocketLaunch { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/rocket_launch{{0}}.wav".BatchNumber(1, 3)
+		);
+
+		public static RandomPicker<string> ScifiLaunch { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/scifi_launch{{0}}.wav".BatchNumber(1, 5)
+		);
+
+		public static RandomPicker<string> ScifiShot { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/scifi_shot.wav"
+        );
+
+		public static RandomPicker<string> SmallLaserShot { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/small_laser_shot.wav"
+		);
+
+		public static RandomPicker<string> SupressedShot { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/normal/weapons/supressed_shot.wav"
+		);
+		
+		
+		
+		
+		// UI
+		public static RandomPicker<string> UiSelect { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/ui/select{{0}}.wav".BatchNumber(1, 4)
+		);
+		
+		public static RandomPicker<string> UiSelectNeutral { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/ui/neutral_select.wav"
+		);
+		
+		public static RandomPicker<string> UiClick { get; } = new RandomPicker<string>(
+			$"{SoundsDir}/ui/click{{0}}.wav".BatchNumber(1, 3)
+		);
+	}
+}
---
+++ src/AbroDraft/Content/Sprites.cs
@@ -0,0 +1,51 @@
+﻿using KludgeBox.Collections;
+
+namespace Game.Content
+{
+	public static class Sprites
+	{
+		public const string TexturesDir = "res://Assets/Textures";
+		public const string SpritesDir = $"{TexturesDir}/Sprites";
+		public const string ParticlesDir = $"{TexturesDir}/Particles";
+
+		// glow
+		public static RandomPicker<string> Spotlight { get; } = new RandomPicker<string>(
+			$"{ParticlesDir}/Glow/256/spotlight_{{0}}.png".BatchNumber(1,8)
+			);
+		public static string Spotlight1 { get; } = $"{ParticlesDir}/Glow/256/spotlight_1.png";
+		public static string Spotlight2 { get; } = $"{ParticlesDir}/Glow/256/spotlight_2.png";
+		public static string Spotlight3 { get; } = $"{ParticlesDir}/Glow/256/spotlight_3.png";
+		public static string Spotlight4 { get; } = $"{ParticlesDir}/Glow/256/spotlight_4.png";
+		public static string Spotlight5 { get; } = $"{ParticlesDir}/Glow/256/spotlight_5.png";
+		public static string Spotlight6 { get; } = $"{ParticlesDir}/Glow/256/spotlight_6.png";
+		public static string Spotlight7 { get; } = $"{ParticlesDir}/Glow/256/spotlight_7.png";
+		public static string Spotlight8 { get; } = $"{ParticlesDir}/Glow/256/spotlight_8.png";
+
+		// star
+		public static RandomPicker<string> Star { get; } = new RandomPicker<string>(
+			$"{ParticlesDir}/Effects/star_0{{0}}.png".BatchNumber(1, 9)
+			);
+		public static string Star1 { get; } = $"{ParticlesDir}/Effects/star_01.png";
+		public static string Star2 { get; } = $"{ParticlesDir}/Effects/star_02.png";
+		public static string Star3 { get; } = $"{ParticlesDir}/Effects/star_03.png";
+		public static string Star4 { get; } = $"{ParticlesDir}/Effects/star_04.png";
+		public static string Star5 { get; } = $"{ParticlesDir}/Effects/star_05.png";
+		public static string Star6 { get; } = $"{ParticlesDir}/Effects/star_06.png";
+		public static string Star7 { get; } = $"{ParticlesDir}/Effects/star_07.png";
+		public static string Star8 { get; } = $"{ParticlesDir}/Effects/star_08.png";
+		public static string Star9 { get; } = $"{ParticlesDir}/Effects/star_09.png";
+
+		// smoke
+		public static RandomPicker<string> Smoke { get; } = new RandomPicker<string>(
+			$"{ParticlesDir}/Effects/smoke_0{{0}}.png".BatchNumber(1, 8)
+			);
+		public static string Smoke1 { get; } = $"{ParticlesDir}/Effects/smoke_01.png";
+		public static string Smoke2 { get; } = $"{ParticlesDir}/Effects/smoke_02.png";
+		public static string Smoke3 { get; } = $"{ParticlesDir}/Effects/smoke_03.png";
+		public static string Smoke4 { get; } = $"{ParticlesDir}/Effects/smoke_04.png";
+		public static string Smoke5 { get; } = $"{ParticlesDir}/Effects/smoke_05.png";
+		public static string Smoke6 { get; } = $"{ParticlesDir}/Effects/smoke_06.png";
+		public static string Smoke7 { get; } = $"{ParticlesDir}/Effects/smoke_07.png";
+		public static string Smoke8 { get; } = $"{ParticlesDir}/Effects/smoke_08.png";
+	}
+}
