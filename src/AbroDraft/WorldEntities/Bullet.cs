677a477b7a02c3e7a3c57a82919b661d9ce3e941
KeyJ148 <kejik148@gmail.com>
2024-01-28 22:04:50 +0300

    Add bullet modulate

--- src/AbroDraft/WorldEntities/Bullet.cs
+++ src/AbroDraft/WorldEntities/Bullet.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using KludgeBox;
 
@@ -13,6 +14,15 @@ public partial class Bullet : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Dictionary<AuthorEnum, Color> colors = new Dictionary<AuthorEnum, Color>()
+		{
+			{ AuthorEnum.PLAYER, new Color(0, 1, 1)},
+			{ AuthorEnum.ALLY, new Color(0, 1, 0)},
+			{ AuthorEnum.ENEMY, new Color(1, 0, 0)}
+			
+		};
+		GetNode<Sprite2D>("Sprite2D").Modulate = colors[Author];
+		
 		GetNode<Area2D>("Area2D").BodyEntered += body =>
 		{
 			if (body is Character character)
