787dad5ae82b0cc67e3d45a9504a24c784f04f22
VasterLordEP <melvik12@ya.ru>
2024-02-10 21:22:16 +0300

    Added reference

--- Scenes/PackedScenesContainer/Main/MainPackedScenesContainer.cs
+++ Scenes/PackedScenesContainer/Main/MainPackedScenesContainer.cs
@@ -4,6 +4,7 @@ public partial class MainPackedScenesContainer : Node
 {
     [Export] [NotNull] public PackedScene MainMenu { get; private set; }
     [Export] [NotNull] public PackedScene BattleWorld { get; private set; }
+    [Export] [NotNull] public PackedScene SafeWorld { get; private set; }
     
     public override void _Ready()
     {
