52e1e30fd2b842d709c5dd0fd0d52275c4d84ed2
Bronuh <bronyx@yandex.ru>
2024-02-11 13:07:55 +0300

    Add new BGM

--- Scenes/World/BattleWorld/BattleWorldService.cs
+++ Scenes/World/BattleWorld/BattleWorldService.cs
@@ -1,5 +1,6 @@
 using Game.Content;
 using Godot;
+using KludgeBox;
 
 public class BattleWorldService
 {
@@ -41,12 +42,26 @@ public class BattleWorldService
         ally.Position = Vec(600, 600);
         battleWorld.AddChild(ally);
         battleWorld.AddChild(battleWorld.Player); // must be here to draw over the floor
-        PlayBattleMusic(); //TODO to music service (battle music service)
+        
+        if (Rand.Chance(0.5))//TODO to music service (battle music service)
+        {
+            PlayBattleMusic1();
+        }
+        else
+        {
+            PlayBattleMusic2();
+        }
     }
     
-    public void PlayBattleMusic()
+    public void PlayBattleMusic1()
     {
-        var music = Audio2D.PlayMusic(Music.WorldBgm, 0.7f);
-        music.Finished += PlayBattleMusic;
+        var music = Audio2D.PlayMusic(Music.WorldBgm1, 0.7f);
+        music.Finished += PlayBattleMusic2;
+    }
+    
+    public void PlayBattleMusic2()
+    {
+        var music = Audio2D.PlayMusic(Music.WorldBgm2, 0.7f);
+        music.Finished += PlayBattleMusic1;
     }
 }
--- Scripts/Content/Music.cs
+++ Scripts/Content/Music.cs
@@ -5,7 +5,11 @@ namespace Game.Content;
 public static class Music
 {
     public const string SoundsDir = "res://Assets/Audio/Music";
-    public static RandomPicker<string> WorldBgm { get; } = new RandomPicker<string>(
+    
+    public static RandomPicker<string> WorldBgm1 { get; } = new RandomPicker<string>(
         $"{SoundsDir}/bgm1.mp3"
+    );
+    public static RandomPicker<string> WorldBgm2 { get; } = new RandomPicker<string>(
+        $"{SoundsDir}/bgm2.mp3"
     );
 }
