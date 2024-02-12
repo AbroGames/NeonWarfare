9e319ef5bf1768f65d90b02150c520aa78372724
Bronuh <bronyx@yandex.ru>
2024-02-12 19:34:39 +0300

    Reset first wave to 0 (1)

--- Scenes/World/BattleWorld/Wave/EnemyWave.cs
+++ Scenes/World/BattleWorld/Wave/EnemyWave.cs
@@ -7,6 +7,6 @@ public class EnemyWave
 	public int OneWaveEnemyCountDelta { get; set; } = 5; 
 	public int WaveTimeout { get; set; } = 12;
 
-	public int WaveNumber { get; set; } = 9;
+	public int WaveNumber { get; set; } = 0;
 	public double NextWaveTimer { get; set; } = 0;
 }
