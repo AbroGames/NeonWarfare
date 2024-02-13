eb1c2501ecd7370fe61bfcead468479304b2444c
Bronuh <bronyx@yandex.ru>
2024-02-13 11:55:27 +0300

    Variate XpOrb sound

--- Scenes/World/Entities/XpOrb/XpOrb.cs
+++ Scenes/World/Entities/XpOrb/XpOrb.cs
@@ -50,10 +50,10 @@ public partial class XpOrb : Node2D
 			EventBus.Publish(new PlayerGainXpEvent(Target, Xp));
 			
 			var label = FloatingLabel.Create($"+{Xp}", Modulate, 0.6);
-			label.Position = Position;
+			label.Position = Position + Rand.InsideUnitCircle * 50;
 			GetParent().AddChild(label);
 
-			Audio2D.PlaySoundAt(Sfx.Beep, Position, 0.5f);
+			Audio2D.PlaySoundAt(Sfx.Beep, Position, 0.35f).PitchVariation(0.25f);
 			
 			QueueFree();
 		}
