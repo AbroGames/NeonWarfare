2e15d7e8cd6da77e5eaddbc609f618f7fc0fdd5f
Bronuh <bronyx@yandex.ru>
2024-02-25 22:09:00 +0300

    Add query helpers for EventBus

---
+++ Scripts/KludgeBox/Events/QueryHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KludgeBox.Events;
+
+public static class QueryHelpers
+{
+    public static Type GetGenericType(Type type)
+    {
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(QueryEvent<>))
+        {
+            return type.GetGenericArguments()[0];
+        }
+
+        Type baseType = type.BaseType;
+        if (baseType != null)
+        {
+            return GetGenericType(baseType);
+        }
+
+        throw new ArgumentException("Тип не является поддерживаемым подтипом QueryEvent<T>.", nameof(type));
+    }
+}
