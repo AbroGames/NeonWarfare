739c1b2f34760dec4c3b201ab90dbd3d479365ef
Bronuh <bronyx@yandex.ru>
2024-03-09 19:55:06 +0300

    Restore wave settings

--- Scenes/World/BattleWorld/Wave/EnemyWave.cs
+++ Scenes/World/BattleWorld/Wave/EnemyWave.cs
@@ -2,9 +2,9 @@ namespace NeoVector;
 
 public class EnemyWave 
 {
-	public int OneWaveEnemyCount { get; set; } = 1; 
-	public int OneWaveEnemyCountDelta { get; set; } = 0; 
-	public int WaveTimeout { get; set; } = 60;
+	public int OneWaveEnemyCount { get; set; } = 30; 
+	public int OneWaveEnemyCountDelta { get; set; } = 1; 
+	public int WaveTimeout { get; set; } = 12;
 
 	public int WaveNumber { get; set; } = 0;
 	public double NextWaveTimer { get; set; } = 0;
