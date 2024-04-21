25f1428a9d3f6610dbebb611ca3954367021eb86
KeyJ148 <kejik148@gmail.com>
2024-04-21 11:41:27 +0300

    Add

--- Scripts/Net/NetworkEntityManager.cs
+++ Scripts/Net/NetworkEntityManager.cs
@@ -7,8 +7,12 @@ public class NetworkEntityManager
 {
     private readonly IDictionary<Node2D, long> _nodeToNid = new Dictionary<Node2D, long>(ReferenceEqualityComparer.Instance);
     private readonly IDictionary<long, Node2D> _nidToNode = new Dictionary<long, Node2D>();
-    private long _nextNid = 0; //TODO поделить диапазон между игроками. Например, по 1ккк каждому новому игроку (если игрок переподключился, то ему дают новый диапазон).
-
+    private long _nextNid = 0; 
+    //TODO поделить диапазон между игроками. Например, по 1ккк каждому новому игроку (если игрок переподключился, то ему дают новый диапазон).
+    //TODO а как менеджить объекты игрока, когда он отключится? На пули можно забить, а турели и остальное? Можно делать менеджемент на клиенте исключительно для снарядов. А остальное на сервере.
+    //TODO а как в террарии? Может просто на стороне сервера создавать пули и не ебаться? Может в пулю добавить id создавшего игрока и reqNid, в ответ передавать Nid и reqNid, чтобы игрок понял, что это его пуля и менеджил ее. При этом nid-ы будут генерироваться только на сервере, получается.
+    //TODO Вынести _nextNid на серверную сторону, на клиенте только два маппинга
+    
     public long AddEntity(Node2D node)
     {
         long nextNid = _nextNid++;
