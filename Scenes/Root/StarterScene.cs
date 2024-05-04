8ae894d91b5f83dfc88c0dab8896169bbe617a90
KeyJ148 <kejik148@gmail.com>
2024-05-04 20:28:13 +0300

    Add StarterScene

--- Scenes/Root/StarterScene.cs
+++ Scenes/Root/StarterScene.cs
@@ -1,19 +1,25 @@
-
+using System.Linq;
 using Godot;
 using KludgeBox;
+using NeoVector;
 
 namespace NeonWarfare;
 
 public partial class StarterScene : Node2D
 {
-	[Export] public PackedScene Scene { get; private set; }
+	
+	[Export] [NotNull] public PackedScene ClientRoot { get; private set; }
+	[Export] [NotNull] public PackedScene ServerRoot { get; private set; }
+	
 	public override void _Ready()
 	{
+		NotNullChecker.CheckProperties(this);
         CallDeferred(nameof(StartGame));
 	}
 
 	private void StartGame()
 	{
-		GetTree().ChangeSceneToPacked(Scene);
+		PackedScene rootScene = OS.GetCmdlineArgs().Contains(ServerParams.ServerFlag) ? ServerRoot : ClientRoot;
+		GetTree().ChangeSceneToPacked(rootScene);
 	}
 }
