3e30efcf2c53377f654e03a49d75b5d9846210ab
Bronuh <bronyx@yandex.ru>
2024-05-04 19:42:19 +0300

    Defer scene change

--- Scenes/Root/StarterScene.cs
+++ Scenes/Root/StarterScene.cs
@@ -1,4 +1,6 @@
+
 using Godot;
+using KludgeBox;
 
 namespace NeonWarfare;
 
@@ -6,6 +8,11 @@ public partial class StarterScene : Node2D
 {
 	[Export] public PackedScene Scene { get; private set; }
 	public override void _Ready()
+	{
+        CallDeferred(nameof(StartGame));
+	}
+
+	private void StartGame()
 	{
 		GetTree().ChangeSceneToPacked(Scene);
 	}
