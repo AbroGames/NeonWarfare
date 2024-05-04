eedbe0bbfb13aa50c790da886faf97e85b47223b
KeyJ148 <kejik148@gmail.com>
2024-05-04 20:23:21 +0300

    Upgrade NotNullChecker

--- Scripts/KludgeBox/Core/NotNullChecker.cs
+++ Scripts/KludgeBox/Core/NotNullChecker.cs
@@ -1,16 +1,28 @@
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using Godot.Collections;
 
 namespace KludgeBox;
 
 public static class NotNullChecker
 {
+
+    private static readonly HashSet<Type> Checked = new();
+    
     public static void CheckProperties(object obj)
+    {
+        if (Checked.Contains(obj.GetType())) return;
+        CheckPropertiesForce(obj);
+    }
+    
+    public static void CheckPropertiesForce(object obj)
     {
         if (obj == null) throw new ArgumentNullException(nameof(obj));
 
         Type type = obj.GetType();
+        Log.Info("Type: " + type);
         foreach (PropertyInfo property in type.GetProperties())
         {
             bool isNull = property.GetValue(obj) == null;
@@ -20,5 +32,7 @@ public static class NotNullChecker
                 Log.Critical($"Property '{property.Name}' in type '{obj.GetType()}' is null, but has NotNull attribute");
             }
         }
+
+        Checked.Add(type);
     }
 }
