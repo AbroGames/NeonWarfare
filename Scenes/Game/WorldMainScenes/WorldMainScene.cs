9550f43ed95145aaea03637777ae8c707e5f9fb0
Evgeniy Serov <kejik148@gmail.com>
2024-07-14 22:43:30 +0300

    Refactoring WorldMainScene

---
+++ Scenes/Game/WorldMainScenes/WorldMainScene.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+using NeonWarfare;
+
+public partial class WorldMainScene : Node2D
+{
+	public World World { get; protected set; }
+	public Hud Hud { get; protected set; }
+}
