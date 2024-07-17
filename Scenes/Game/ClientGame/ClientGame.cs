5c77f4385c5af403d0001e3d3f0d5ba8026ac7f1
Evgeniy Serov <kejik148@gmail.com>
2024-07-17 22:45:09 +0300

    Create Game scene

---
+++ Scenes/Game/ClientGame/ClientGame.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+using KludgeBox;
+
+public partial class ClientGame : Game
+{
+	
+	public override void _Ready()
+	{
+		base._Ready();
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
