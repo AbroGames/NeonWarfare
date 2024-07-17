59b547fab34808a642a44fb21ed3f67fad71abbf
Evgeniy Serov <kejik148@gmail.com>
2024-07-17 22:44:27 +0300

    Refactoring MainPackedScenesContainer

--- Scenes/Root/PackedScenesContainer/Main/MainPackedScenesContainer.cs
+++ Scenes/Root/PackedScenesContainer/Main/MainPackedScenesContainer.cs
@@ -6,6 +6,8 @@ namespace NeonWarfare;
 public partial class MainPackedScenesContainer : Node
 {
     [Export] [NotNull] public PackedScene MainMenu { get; private set; }
+    [Export] [NotNull] public PackedScene ClientGame { get; private set; }
+    [Export] [NotNull] public PackedScene ServerGame { get; private set; }
     [Export] [NotNull] public PackedScene BattleWorld { get; private set; }
     [Export] [NotNull] public PackedScene SafeWorld { get; private set; }
     
