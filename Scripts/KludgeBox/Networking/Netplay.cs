c25888cc53e287b4390f206055a75296d135e995
Bronuh <bronyx@yandex.ru>
2024-07-20 13:26:53 +0300

    Update Netplay - rescan packets on ResetConnection() - add more parameters for Send() method - add SendRaw() method - add equality check for Initialize() to avoid multiple subscriptions to events - add more detailed XML docs

--- Scripts/KludgeBox/Networking/Netplay.cs
+++ Scripts/KludgeBox/Networking/Netplay.cs
@@ -12,6 +12,7 @@ public enum Netmode
     Client,
     Server
 }
+
 public static class Netplay
 {
     public const long ServerId = 1;
@@ -55,6 +56,7 @@ public static class Netplay
     {
         Mode = Netmode.Client;
         ResetConnection();
+        
         var peer = new ENetMultiplayerPeer();
         peer.CreateClient(host, port);
         
@@ -70,37 +72,65 @@ public static class Netplay
     public static void ResetConnection()
     {
         Peer?.Close();
+        PacketRegistry.ScanPackets();
     }
-
+    
     /// <summary>
-    /// Sends packet to all available peers.
+    /// Broadcasts packet to all available peers.
     /// </summary>
-    public static void Send(NetPacket packet)
+    /// <param name="packet">Packet to send</param>
+    /// <param name="mode">Reliability mode (Reliable, Unreliable or UnreliableOrdered)</param>
+    /// <param name="channel">Channel to send on. -1 means use packet's preferred channel</param>
+    public static void Send(NetPacket packet, MultiplayerPeer.TransferModeEnum mode = MultiplayerPeer.TransferModeEnum.Reliable, int channel = -1)
     {
-        Send(BroadcastId, packet);
+        Send(BroadcastId, packet, mode, channel);
     }
     
     /// <summary>
     /// Sends packet to specified peer.
     /// </summary>
-    /// <param name="id">ID of the peer. 0 for broadcast and 1 for server.</param>
-    public static void Send(long id, NetPacket packet)
+    /// <param name="id">Peer id to send to. 0 for broadcast, 1 for server.</param>
+    /// <param name="packet">Packet to send</param>
+    /// <param name="mode">Reliability mode (Reliable, Unreliable or UnreliableOrdered)</param>
+    /// <param name="channel">Channel to send on. -1 means use packet's preferred channel</param>
+    public static void Send(long id, NetPacket packet, MultiplayerPeer.TransferModeEnum mode = MultiplayerPeer.TransferModeEnum.Reliable, int channel = -1)
     {
         var bytes = PacketHelper.EncodePacket(packet);
-        //Log.Info($"Sending packet to {id} with {packetData.Length} + 4 bytes. Expected type is {packet.GetType().FullName}");
-        Api.SendBytes(bytes, (int)id, MultiplayerPeer.TransferModeEnum.Reliable, packet.PreferredChannel);
+        channel = channel < 0 ? packet.PreferredChannel : channel;
+
+        SendRaw(id, bytes, mode, channel);
+    }
+
+    /// <summary>
+    /// Sends raw bytes to specified peer. Receiving peer will try to decode it anyways.
+    /// </summary>
+    /// <param name="id">Peer id to send to. 0 for broadcast, 1 for server.</param>
+    /// <param name="encodedPacketBuffer">Expected to be an encoded packet (like PacketHelper.EncodePacket(packet.ToBuffer()))</param>
+    /// <param name="mode">Reliability mode (Reliable, Unreliable or UnreliableOrdered)</param>
+    /// <param name="channel">Channel to send on. -1 means use packet's preferred channel</param>
+    /// <remarks>
+    /// This can be used to avoid multiple calls to <see cref="PacketHelper.EncodePacket"/> and <see cref="NetPacket.ToBuffer"/> for sending one packet to multiple specified peers.
+    /// </remarks>
+    public static void SendRaw(long id, byte[] encodedPacketBuffer, MultiplayerPeer.TransferModeEnum mode = MultiplayerPeer.TransferModeEnum.Reliable, int channel = 0)
+    {
+        Api.SendBytes(encodedPacketBuffer, (int)id, mode, channel);
     }
 
     // Called from Root._Ready()
+    // Seems like some sort of potential memory leak.
     internal static void Initialize(SceneMultiplayer api)
     {
+        // Avoid multiple event subscriptions.
+        if(Api == api)
+            return;
+        
         Api = api;
         Api.ConnectedToServer += () => ConnectedToServer?.Invoke();
         Api.ConnectionFailed += () => ConnectionFailed?.Invoke();
         Api.PeerConnected += id => PeerConnected?.Invoke(id);
         Api.PeerDisconnected += id => PeerDisconnected?.Invoke(id);
         Api.PeerPacket += OnPacketReceived;
-        
+
         ResetConnection();
     }
     
