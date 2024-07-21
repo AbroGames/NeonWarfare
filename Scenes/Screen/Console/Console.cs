9bf124b75c0cfb06a4666bc4d235da523cb42692
Bronuh <bronyx@yandex.ru>
2024-07-21 12:29:21 +0300

    Rewrite console to use Labels instead of RichTextLabel. - add setting to adjust maximum amount of messages in console

--- Scenes/Screen/Console/Console.cs
+++ Scenes/Screen/Console/Console.cs
@@ -7,7 +7,11 @@ namespace NeonWarfare;
 
 public partial class Console : CanvasLayer, ILogger
 {
-    [Export] [NotNull] public RichTextLabel TextLabel { get; private set; }
+    //[Export] [NotNull] public RichTextLabel TextLabel { get; private set; }
+    [Export] [NotNull] public VBoxContainer MessagesContainer { get; private set; }
+    [Export] [NotNull] public ScrollContainer ScrollContainer { get; private set; }
+    [Export] public int MessagesCount { get; set; } = 1000;
+    [Export] public int FontSize { get; set; } = 10;
     
     public override void _Ready()
     {
@@ -38,8 +42,54 @@ public partial class Console : CanvasLayer, ILogger
     {
         Print(msg, Colors.DarkRed, exception);
     }
-    
+
     private void Print(object msg, Color color, Exception exception = null)
+    {
+        if(msg is null && exception is null)
+        {
+            AddMessage("", Col());
+            return;
+        }
+        
+        var sb = new StringBuilder();
+        sb.Append(msg ?? "");
+        sb.Append(msg is null || exception is null ? "" : "\n");
+        sb.Append(exception?.StackTrace ?? "");
+        
+        AddMessage(sb.ToString(), color);
+    }
+
+    private void AddMessage(string text, Color color)
+    {
+        var scroll = ScrollContainer.GetVScrollBar().Value;
+        var maxScroll = ScrollContainer.GetVScrollBar().MaxValue;
+        
+        var label = new Label
+        {
+            Text = text
+        };
+        label.Modulate = color;
+        label.LabelSettings = new();
+        label.LabelSettings.FontSize = FontSize;
+        MessagesContainer.AddChild(label);
+
+        if (maxScroll != 0)
+        {
+            var scrollPercent = scroll / maxScroll;
+            if (scrollPercent is >= 0.98 or 0.0)
+            {
+                // Scroll to bottom
+                ScrollContainer.ScrollVertical = (int)ScrollContainer.GetVScrollBar().MaxValue;
+            }
+        }
+
+        if (MessagesContainer.GetChildCount() > MessagesCount)
+        {
+            MessagesContainer.GetChild(0).QueueFree();
+        }
+    }
+    
+    /*private void Print(object msg, Color color, Exception exception = null)
     {
         if(msg is null && exception is null)
         {
@@ -55,5 +105,6 @@ public partial class Console : CanvasLayer, ILogger
         TextLabel.PushColor(color);
         TextLabel.AppendText($"\n{sb}");
         TextLabel.ScrollToLine(TextLabel.GetLineCount());
-    }
+        
+    }*/
 }
