2ba89bd4643d796bd1b7b6ed36f2164603b1ab29
Evgeniy Serov <kejik148@gmail.com>
2024-07-22 01:03:19 +0300

    Refactoring PackedScenesContainer

--- Scenes/PackedScenesContainer/Effects/EffectsPackedScenesContainer.cs
+++ Scenes/PackedScenesContainer/Effects/EffectsPackedScenesContainer.cs
@@ -9,6 +9,7 @@ public partial class EffectsPackedScenesContainer : Node
 	[Export] [NotNull] public PackedScene DebrisFx { get; private set; }
 	[Export] [NotNull] public PackedScene BulletHitFx { get; private set; }
 	[Export] [NotNull] public PackedScene SpawnFx { get; private set; }
+	
 	public override void _Ready()
 	{
 		NotNullChecker.CheckProperties(this);
