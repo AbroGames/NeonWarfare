e38a55c92e5ad25edc7122fcdd82b1e22c9d1a80
Bronuh <bronyx@yandex.ru>
2024-07-21 11:51:29 +0300

    Intercept unhandled exceptions to Log.Error

--- Scenes/Root/StarterScene/StarterScene.cs
+++ Scenes/Root/StarterScene/StarterScene.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics;
 using Godot;
 using KludgeBox;
 using NeonWarfare.Net;
@@ -19,7 +21,26 @@ public partial class StarterScene : Node
 
 	private void StartGame()
 	{
+		AppDomain.CurrentDomain.UnhandledException += HandleException;
 		PackedScene rootScene = CmdArgsService.ContainsInCmdArgs(ServerParams.ServerFlag) ? ServerRoot : ClientRoot;
 		GetTree().ChangeSceneToPacked(rootScene);
+	}
+
+	private static void HandleException(object sender, UnhandledExceptionEventArgs args)
+	{
+		// If logging will produce unhandled exception then we fucked up, so we need try/catch here.
+		try
+		{
+			if (args.ExceptionObject is not Exception exception) return;
+			
+			Log.Error(exception.ToString());
+		}
+		catch (Exception e)
+		{
+			// Use GD.Print instead of Log.Error to avoid infinite recursion.
+			GD.Print($"Unexpected exception was thrown while handling unhandled exception: {e}");
+		}
+		
+		Debugger.Break();
 	}
 }
