c84d4dda5a6d821153553b592eb61205303b0eac
Evgeniy Serov <kejik148@gmail.com>
2024-07-28 18:12:34 +0300

    Refactoring PackedScenesContainer

---
+++ Scenes/PackedScenesContainer/Client/World/ClientWorldPackedScenesContainer.cs
@@ -0,0 +1,17 @@
+using Godot;
+using KludgeBox;
+
+namespace NeonWarfare;
+
+public partial class ClientWorldPackedScenesContainer : Node
+{
+    [Export] [NotNull] public PackedScene SafeWorld { get; private set; }
+    [Export] [NotNull] public PackedScene BattleWorld { get; private set; }
+    
+    [Export] [NotNull] public PackedScene FloatingLabel { get; private set; }
+    
+    public override void _Ready()
+    {
+        NotNullChecker.CheckProperties(this);
+    }
+}
