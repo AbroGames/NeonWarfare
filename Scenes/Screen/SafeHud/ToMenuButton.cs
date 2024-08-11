f3abd396172ae4f29cda9aa40edd9802ef9cdcd8
Evgeniy Serov <kejik148@gmail.com>
2024-08-11 21:54:19 +0300

    Add ToMainMenu button

---
+++ Scenes/Screen/SafeHud/ToMenuButton.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+using KludgeBox.Networking;
+using NeonWarfare.Net;
+
+namespace NeonWarfare;
+
+public partial class ToMenuButton : Button
+{
+    
+    public override void _Ready()
+    {
+        Pressed += () =>
+        {
+            //TODO по хорошему провести подготовительные действия, отправить на сервер сигнал, закрыть нормально подключение и т.п. Все это в отдельном методе, предположительно в ClientGameNetwork.cs
+            ClientRoot.Instance.CreateMainMenu();
+        };
+    }
+
+}
