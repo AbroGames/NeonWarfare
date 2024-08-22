541686cce0679bc848866f40ed575e0bb26b3795
Evgeniy Serov <kejik148@gmail.com>
2024-08-22 23:09:00 +0300

    Move listeners to NetworkEntity

--- Scenes/World/Entities/NetworkEntity.cs
+++ Scenes/World/Entities/NetworkEntity.cs
@@ -1,8 +1,24 @@
 ﻿using Godot;
+using KludgeBox.Events;
+using NeonWarfare.NetOld.Server;
 
 namespace NeonWarfare;
 
 public partial class NetworkEntity : Node2D
 {
-    
+    /* TODO
+    [EventListener(ListenerSide.Client)]
+    public static void OnServerPositionEntityPacket(ServerPositionEntityPacket serverPositionEntityPacket)
+    {
+        NetworkEntity entity = ClientRoot.Instance.Game.World.NetworkEntityManager.GetNode<NetworkEntity>(serverPositionEntityPacket.Nid);
+        entity.Position = Vec(serverPositionEntityPacket.X, serverPositionEntityPacket.Y);
+        entity.Rotation = serverPositionEntityPacket.Dir;
+    }
+
+    [EventListener(ListenerSide.Client)]
+    public static void OnServerDestroyEntityPacket(ServerDestroyEntityPacket serverDestroyEntityPacket)
+    {
+        NetworkEntity entity = ClientRoot.Instance.Game.World.NetworkEntityManager.RemoveEntity<NetworkEntity>(serverDestroyEntityPacket.Nid);
+        entity.QueueFree();
+    }*/
 }
