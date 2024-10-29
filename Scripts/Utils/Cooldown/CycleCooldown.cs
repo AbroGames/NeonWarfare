4ca709fccf285f3d3e785f6fc225fcc03c96b497
Evgeniy Serov <kejik148@gmail.com>
2024-10-29 18:31:34 +0500

    Create CycleCooldown

---
+++ Scripts/Utils/Cooldown/CycleCooldown.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace NeonWarfare.Utils.Cooldown;
+
+public class CycleCooldown : Cooldown
+{
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CycleCooldown"/> class with the specified duration.
+    /// </summary>
+    /// <param name="duration">The duration of the cooldown in seconds.</param>
+    public CycleCooldown(double duration, bool isActivated = true, Action actionWhenReady = null) : 
+        base(duration, isActivated, actionWhenReady) { }
+
+    /// <summary>
+    /// Updates the cooldown by a specified delta time 
+    /// </summary>
+    /// <param name="deltaTime">The time elapsed since the last update in seconds.</param>
+    public void Update(double deltaTime)
+    {
+        if (!_isActivated) return;
+		
+        _timeLeft -= deltaTime;
+        while (_timeLeft <= 0)
+        {
+            _timeLeft += Duration;
+            ActivateAction();
+        }
+    }
+	
+    /// <summary>
+    /// Resetting the elapsed time to 0 and stop cooldowner.
+    /// </summary>
+    public void Reset()
+    {
+        _timeLeft = Duration;
+        _isActivated = false;
+    }
+
+    /// <summary>
+    /// Resetting the elapsed time to 0 and activate cooldowner.
+    /// </summary>
+    public void Restart()
+    {
+        Reset();
+        _isActivated = true;
+    }
+
+    public void Start()
+    {
+        _isActivated = true;
+    }
+
+    public void Pause()
+    {
+        _isActivated = false;
+    }
+}
