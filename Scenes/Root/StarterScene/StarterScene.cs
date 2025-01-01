23dcbca3de15c638ab6d6e1ed2d1172c33dc7299
Bronuh <bronyx@yandex.ru>
2025-01-01 11:30:27 +0300

    Add Harmony patch for Godot.NativeInterop.ExceptionUtils.LogException(e) utility method

--- Scenes/Root/StarterScene/StarterScene.cs
+++ Scenes/Root/StarterScene/StarterScene.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using Godot;
+using HarmonyLib;
 using KludgeBox;
 using NeonWarfare.Utils;
 
@@ -12,8 +13,26 @@ public partial class StarterScene : Node
 	[Export] [NotNull] public PackedScene ClientRoot { get; private set; }
 	[Export] [NotNull] public PackedScene ServerRoot { get; private set; }
 	
+	private static void PatchLogException()
+	{
+		var harmony = new Harmony("cc.abro.patch");
+		var exceptionUtilsType = AccessTools.TypeByName("Godot.NativeInterop.ExceptionUtils");
+		var originalMethod = AccessTools.Method(exceptionUtilsType, "LogException", [typeof(Exception)]);
+		var patchMethod = AccessTools.Method(typeof(StarterScene), nameof(PatchPrefix));
+
+		harmony.Patch(originalMethod, new HarmonyMethod(patchMethod), null);
+		Log.Info($"Applied patch for {originalMethod.DeclaringType!.FullName}.{originalMethod.Name} to log exceptions");
+	}
+
+	public static bool PatchPrefix(ref Exception e)
+	{
+		Log.Error(e.ToString());
+		return false;
+	}
+	
 	public override void _Ready()
 	{
+		PatchLogException();
 		NotNullChecker.CheckProperties(this);
         CallDeferred(nameof(StartGame));
 	}
