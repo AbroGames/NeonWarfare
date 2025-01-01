1892aea2b038821a2c42870c4297d444bb0ed0e6
Evgeniy Serov <kejik148@gmail.com>
2025-01-02 01:34:20 +0500

    #7 Refactoring "World" folder files

--- Scenes/NewWorld/Effects/CpuParticlesFx.cs
+++ Scenes/NewWorld/Effects/CpuParticlesFx.cs
@@ -6,11 +6,15 @@ namespace NeonWarfare;
 
 public partial class CpuParticlesFx : Node2D
 {
-	public event Action Finished;
+	
 	[Export] [NotNull] public CpuParticles2D Particles2D { get; private set; }
+	
+	public event Action Finished;
+	
 	public override void _Ready()
 	{
 		NotNullChecker.CheckProperties(this);
+		
 		Particles2D.Emitting = true;
 		Particles2D.Finished += () =>
 		{
--- Scenes/NewWorld/Effects/GpuParticlesFx.cs
+++ Scenes/NewWorld/Effects/GpuParticlesFx.cs
@@ -6,11 +6,15 @@ namespace NeonWarfare;
 
 public partial class GpuParticlesFx : Node2D
 {
-	public event Action Finished;
+	
 	[Export] [NotNull] public GpuParticles2D Particles2D { get; private set; }
+	
+	public event Action Finished;
+	
 	public override void _Ready()
 	{
 		NotNullChecker.CheckProperties(this);
+		
 		Particles2D.Emitting = true;
 		Particles2D.Finished += () =>
 		{
