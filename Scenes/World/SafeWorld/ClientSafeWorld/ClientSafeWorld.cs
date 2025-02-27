fee7538814052ca83ef3f722195a72c125453783
Evgeniy Serov <kejik148@gmail.com>
2025-02-27 22:07:19 +0500

    Add test version MapGenerator

--- Scenes/World/SafeWorld/ClientSafeWorld/ClientSafeWorld.cs
+++ Scenes/World/SafeWorld/ClientSafeWorld/ClientSafeWorld.cs
@@ -1,3 +1,7 @@
+using System;
+using NeonWarfare.Scripts.KludgeBox.Core;
+using NeonWarfare.Scripts.Utils.MapGenerator;
+
 namespace NeonWarfare.Scenes.World.SafeWorld.ClientSafeWorld;
 
 public partial class ClientSafeWorld : ClientWorld
@@ -7,5 +11,12 @@ public partial class ClientSafeWorld : ClientWorld
 	{
 		base._Ready();
 		PlaySafeMusic();
+		
+		//TODO перенести на серверную сторону, и в боевой мир вместо мирного хаба
+		MapGenerator mapGenerator = new MapGenerator();
+		foreach (SC_StaticEntitySpawnPacket entitySpawnPacket in mapGenerator.Generate())
+		{
+			OnStaticEntitySpawnPacket(entitySpawnPacket);
+		}
 	}
 }
--- Scripts/Utils/MapGenerator/MapGenerator.cs
+++ Scripts/Utils/MapGenerator/MapGenerator.cs
@@ -1,9 +1,35 @@
+using System.Collections.Generic;
+using Godot;
+using NeonWarfare.Scenes.World;
+using NeonWarfare.Scripts.KludgeBox;
+using NeonWarfare.Scripts.KludgeBox.Core;
+
 namespace NeonWarfare.Scripts.Utils.MapGenerator;
 
 public class MapGenerator
 {
+
+    public readonly long Seed;
+    
+    public MapGenerator() : this((long) Rand.Int * Rand.Int) { }
+    
     public MapGenerator(long seed)
     {
-    
+        Seed = seed;
+    }
+
+    /*
+     TODO Временно возвращаем SC_StaticEntitySpawnPacket.
+     В целевой версии должны возвращать DTO-прослойку, которая потом будет использована для:
+        1) Создание SC_StaticEntitySpawnPacket и отправка на клиент
+        2) Создание ноды-сцены и спаун её на сервере
+     */
+    public List<ClientWorld.SC_StaticEntitySpawnPacket> Generate()
+    {
+        return new List<ClientWorld.SC_StaticEntitySpawnPacket>
+        {
+            new ClientWorld.SC_StaticEntitySpawnPacket(ClientWorld.SC_StaticEntitySpawnPacket.StaticEntityType.Wall, new Vector2(200, 200), 0),
+            new ClientWorld.SC_StaticEntitySpawnPacket(ClientWorld.SC_StaticEntitySpawnPacket.StaticEntityType.Wall, new Vector2(-200, -200), 0)
+        };
     }
 }
