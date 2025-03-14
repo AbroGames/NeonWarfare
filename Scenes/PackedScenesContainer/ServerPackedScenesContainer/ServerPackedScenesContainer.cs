4393ddbdd194041dcdbcedf954f55373755bb464
Evgeniy Serov <kejik148@gmail.com>
2025-03-14 15:01:37 +0500

    Add Wall scene to server-side

--- Scenes/PackedScenesContainer/ServerPackedScenesContainer/ServerPackedScenesContainer.cs
+++ Scenes/PackedScenesContainer/ServerPackedScenesContainer/ServerPackedScenesContainer.cs
@@ -16,6 +16,8 @@ public partial class ServerPackedScenesContainer : Node
     [Export] [NotNull] public PackedScene Player { get; private set; }
     [Export] [NotNull] public PackedScene Enemy { get; private set; }
     
+    [Export] [NotNull] public PackedScene Wall { get; private set; }
+    
     public override void _Ready()
     {
         NotNullChecker.CheckProperties(this);
