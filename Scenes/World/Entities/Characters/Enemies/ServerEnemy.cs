ad69da015bc0022f7f643a65e3fc41e7e3b42f1a
Bronuh <bronyx@yandex.ru>
2025-03-18 12:55:16 +0300

    Add basic AI to ServerEnemy

--- Scenes/World/Entities/Characters/Enemies/ServerEnemy.cs
+++ Scenes/World/Entities/Characters/Enemies/ServerEnemy.cs
@@ -30,6 +30,9 @@ public partial class ServerEnemy : ServerCharacter
         };
         rotateComponent.GetRotationSpeedFunc = () => RotationSpeed;
         AddChild(rotateComponent);
+
+        var enemyAi = new ServerEnemyAiComponent();
+        AddChild(enemyAi);
     }
 
     public void InitOnSpawn(Vector2 position, float rotation)
@@ -94,5 +97,32 @@ public partial class ServerEnemy : ServerCharacter
     public void TryUseSkill(long skillId)
     {
         TryUseSkill(skillId, Position, Rotation, GetGlobalMousePosition(), -1);
+    }
+
+    /// <summary>
+    /// Эта штука должна вернуть радиус юнита в контексте поиска пути. В радиус этой окружности должно вписываться физическое тело врага (не хитбокс).
+    /// По умолчанию пытается получить радиус окружности, вписанной в ограничительную рамку физической формы (ты еще читаешь?) юнита. На выходе должна получаться исходная окружность, наверное.
+    /// </summary>
+    /// <returns>Примерный размер юнита</returns>
+    /// <remarks>
+    /// TODO: Жыамбек, ты что-то говорил про виртуальный метод с размерами или чет такое. Вот, переопределяй если надо.
+    /// </remarks>
+    public virtual float GetNavigationRadius()
+    {
+        return CollisionShape.Shape.GetRect().Size.X // ширина прямоугольника, описанного вокруг формы, БЕЗ учета Scale
+            / 2 // половина этой ширины будет равна радиусу окружности
+            * 1.05f; // небольшой запасик на всякий случай
+    }
+
+    /// <summary>
+    /// Этот метод должен вернуть дальность зрения или внимания врага, в пределах которого он должен повернуться к своей цели
+    /// </summary>
+    /// <returns>Радиус поля зрения врага</returns>
+    /// <remarks>
+    /// TODO: Жижа, этот метод тоже для тебя, пользуйся
+    /// </remarks>
+    public virtual float GetEnemyReachRange()
+    {
+        return 2000; // Самое большое значение, которое я нашел в скиллах
     }
 }
