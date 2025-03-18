5efd86106a1c7774ebf218c736dc09ffa1fc6449
Bronuh <bronyx@yandex.ru>
2025-03-18 18:06:09 +0300

    Implement dynamic 3D background in main menu

---
+++ Scenes/Screen/MainScene/Background3D.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+using NeonWarfare.Scripts.KludgeBox.Core;
+
+public partial class Background3D : Node3D
+{
+    [Export] [NotNull] private OmniLight3D _light { get; set; }
+    private Vector3 _startLightPosition = new(-4, 0, 15);
+    private Vector3 _endLightPosition = new(-4, 0, -20);
+    private float _lightSpeed = 10f;
+
+    private float ExpectedTraverseTime => _startLightPosition.DistanceTo(_endLightPosition) / _lightSpeed;
+    public override void _Ready()
+    {
+        NotNullChecker.CheckProperties(this);
+        PlayLightAnimation();
+    }
+
+    private void PlayLightAnimation()
+    {
+        _light.Position = _startLightPosition;
+        var tween = CreateTween();
+        tween.TweenProperty(_light, "position", _endLightPosition, ExpectedTraverseTime);
+        tween.TweenCallback(Callable.From(PlayLightAnimation));
+        tween.Play();
+    }
+
+}
