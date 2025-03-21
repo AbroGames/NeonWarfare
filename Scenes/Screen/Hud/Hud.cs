c07d880b20f766385ab7010143a83d18421c3394
Bronuh <bronyx@yandex.ru>
2025-03-21 14:19:51 +0300

    Add some death effects

--- Scenes/Screen/Hud/Hud.cs
+++ Scenes/Screen/Hud/Hud.cs
@@ -11,6 +11,7 @@ using NeonWarfare.Scenes.World.Entities.Characters.Players;
 using NeonWarfare.Scripts.Content.Skills;
 using NeonWarfare.Scripts.KludgeBox;
 using NeonWarfare.Scripts.KludgeBox.Core;
+using NeonWarfare.Scripts.KludgeBox.Godot.Services;
 
 namespace NeonWarfare.Scenes.Screen;
 
@@ -26,10 +27,18 @@ public partial class Hud : Control
 	[ExportGroup("Bottom part")]
 	[Export] [NotNull] public HBoxContainer SkillsContainer { get; private set; }
 	
+	[ExportGroup("General")]
+	[Export] [NotNull] public DeathOverlay DeathOverlay { get; private set; }
+	
 	
 	private readonly Stopwatch _physicsStopwatch = new();
 	private readonly Queue<double> _deltas = new();
 	private bool _isPlayerInitialized;
+	private float _deathOverlayAnimationTime = 2f;
+	private float _deathEffectStrength;
+	private float _previousDeathEffectStrength = -1;
+
+	private float[] _deathEqGains = [2f, 6.8f, 6f, 3.5f, 0.1f, -8.4f, -10.1f, -11.5f, -12f, -12f];
 	public virtual ClientPlayer GetCurrentPlayer()
 	{
 		return GetCurrentWorld().Player;
@@ -39,7 +48,7 @@ public partial class Hud : Control
 	{
 		throw new NotSupportedException();
 	}
-
+	
 	public override void _Process(double delta)
 	{
 		Fps.Text = $"FPS: {Engine.GetFramesPerSecond():N0}";
@@ -58,6 +67,26 @@ public partial class Hud : Control
 		HpBar.MaxValue = (float) player.MaxHp;
 		HpBar.Label.Text = $"Health: {player.Hp:N0} / {player.MaxHp:N0}";
 		Fps.Text = $"FPS: {Engine.GetFramesPerSecond():N0}";
+
+		if (player.IsDead)
+		{
+			_deathEffectStrength += (float)delta / _deathOverlayAnimationTime;
+		}
+		else
+		{
+			_deathEffectStrength -= (float)delta / _deathOverlayAnimationTime;
+		}
+		
+		_deathEffectStrength = Math.Clamp(_deathEffectStrength, 0, 1);
+		
+		// ReSharper disable once CompareOfFloatsByEqualityOperator
+		if (_deathEffectStrength != _previousDeathEffectStrength)
+		{
+			DeathOverlay.SetStrength(_deathEffectStrength);
+			AdjustSoundMuffleStrength(_deathEffectStrength);
+		}
+		
+		_previousDeathEffectStrength = _deathEffectStrength;
 	}
 	
 	public override void _PhysicsProcess(double delta)
@@ -72,6 +101,24 @@ public partial class Hud : Control
 			_deltas.Dequeue();
 		}
 		_physicsStopwatch.Restart();
+	}
+
+	public override void _ExitTree()
+	{
+		AdjustSoundMuffleStrength(0);
+	}
+
+	private void AdjustSoundMuffleStrength(float strenght)
+	{
+		const float additionalStrenght = 2f;
+		var effect = AudioServer.GetBusEffect(Audio2D.MasterIndex, 0);
+		if (effect is AudioEffectEQ10 eq)
+		{
+			for (int i = 0; i < _deathEqGains.Length; i++)
+			{
+				eq.SetBandGainDb(i, _deathEqGains[i] * strenght * additionalStrenght);
+			}
+		}
 	}
 
 	private void InitHudForPlayer()
