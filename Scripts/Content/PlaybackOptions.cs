9b26ccbbf45f560aa2e9d81aaa4858814fe01597
Bronuh <bronyx@yandex.ru>
2025-03-25 15:05:09 +0300

    Decrease sounds panning

--- Scripts/Content/PlaybackOptions.cs
+++ Scripts/Content/PlaybackOptions.cs
@@ -3,7 +3,7 @@
 public record PlaybackOptions(string Path, 
     float Volume,
     float MaxDistance = 3000f,
-    float PanningStrength = 3f,
+    float PanningStrength = 2f,
     float Attenuation = 1f,
     float PitchScale = 1f
 );
