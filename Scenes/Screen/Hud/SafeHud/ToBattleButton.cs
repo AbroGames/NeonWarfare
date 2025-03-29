bc7cfbb2254953d069558c5b3c8ac21ee30726b9
Bronuh <bronyx@yandex.ru>
2025-03-29 20:41:39 +0300

    Hide "To Battle" button for not admins

--- Scenes/Screen/Hud/SafeHud/ToBattleButton.cs
+++ Scenes/Screen/Hud/SafeHud/ToBattleButton.cs
@@ -1,5 +1,6 @@
 ﻿using Godot;
 using NeonWarfare.Scenes.Game.ServerGame;
+using NeonWarfare.Scenes.Root.ClientRoot;
 using NeonWarfare.Scripts.KludgeBox.Networking;
 
 namespace NeonWarfare.Scenes.Screen.SafeHud;
@@ -8,6 +9,7 @@ public partial class ToBattleButton : Button
 {
     public override void _Ready()
     {
+        Visible = ClientRoot.Instance.Game.PlayerProfile.IsAdmin;
         Pressed += () =>
         {
             Network.SendToServer(new ServerGame.CS_AdminGoToBattlePacket());
