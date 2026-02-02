594af54b4866d3556b02f2290798dfe01d28eb02
Evgeniy Serov <kejik148@gmail.com>
2026-02-02 23:01:23 +0500

    Implement basic AIController logic

--- Scenes/NeonTemp/Entity/Character/Controller/Ai/Impl/AiBattleControllerLogic.cs
+++ Scenes/NeonTemp/Entity/Character/Controller/Ai/Impl/AiBattleControllerLogic.cs
@@ -1,6 +1,16 @@
-﻿namespace NeonWarfare.Scenes.NeonTemp.Entity.Character.Controller.Ai.Impl;
+﻿using Godot;
+
+namespace NeonWarfare.Scenes.NeonTemp.Entity.Character.Controller.Ai.Impl;
 
 public class AiBattleControllerLogic : IAiControllerLogic
 {
-    
+    public Vector2 GetMovementInput(Character character)
+    {
+        return Vec2(-1, 0);
+    }
+
+    public Vector2 GetGlobalRotatePosition(Character character)
+    {
+        return Vector2.Zero;
+    }
 }
--- Scenes/NeonTemp/Entity/Character/Controller/Ai/Impl/AiPatrolControllerLogic.cs
+++ Scenes/NeonTemp/Entity/Character/Controller/Ai/Impl/AiPatrolControllerLogic.cs
@@ -1,6 +1,26 @@
-﻿namespace NeonWarfare.Scenes.NeonTemp.Entity.Character.Controller.Ai.Impl;
+﻿using Godot;
+
+namespace NeonWarfare.Scenes.NeonTemp.Entity.Character.Controller.Ai.Impl;
 
 public class AiPatrolControllerLogic : IAiControllerLogic
 {
-    
+    public Vector2 GetMovementInput(Character character)
+    {
+        return Services.Rand.UnitVector;
+    }
+
+    public Vector2 GetGlobalRotatePosition(Character character)
+    {
+        return character.Position + Services.Rand.UnitVector * 10;
+    }
+
+    public double GetMovementSpeed(Character character)
+    {
+        return character.Stats.MovementSpeed * 0.2;
+    }
+
+    public double GetRotationSpeed(Character character)
+    {
+        return character.Stats.RotationSpeed * 0.5;
+    }
 }
