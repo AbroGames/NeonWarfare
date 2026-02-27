4e3c54866b23a2bad68adbe22cd03e62b90b8022
Evgeniy Serov <kejik148@gmail.com>
2026-02-27 18:50:25 +0500

    #128 Change AddingPolicy

---
+++ Scenes/NeonTemp/Entity/Character/StatusEffects/AddingPolicy/NoCheckAddingPolicy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeonWarfare.Scenes.NeonTemp.Entity.Character.StatusEffects.AddingPolicy;
+
+public class NoCheckAddingPolicy : IAddingStatusEffectPolicy
+{
+    public void OnAdd(
+        Character character, 
+        Character author, 
+        StatusEffect newStatusEffect,
+        IReadOnlyList<StatusEffect> currentStatusEffects,
+        IReadOnlyDictionary<string, List<StatusEffect>> currentStatusEffectsById,
+        IReadOnlyDictionary<string, List<StatusEffect>> currentStatusEffectsByTag,
+        Action<StatusEffect, Character> addStatusEffectFunc, 
+        Action<StatusEffect> removeStatusEffectFunc)
+    {
+        addStatusEffectFunc(newStatusEffect, author);
+    }
+}
