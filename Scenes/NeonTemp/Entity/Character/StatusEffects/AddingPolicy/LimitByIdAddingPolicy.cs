9aa57337c7113ab77bc438d29a3bf3ea28cf5a31
Evgeniy Serov <kejik148@gmail.com>
2026-02-27 18:51:04 +0500

    #128 Add new AddingPolicy

---
+++ Scenes/NeonTemp/Entity/Character/StatusEffects/AddingPolicy/LimitByIdAddingPolicy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeonWarfare.Scenes.NeonTemp.Entity.Character.StatusEffects.AddingPolicy;
+
+public class LimitByIdAddingPolicy(int limit = 1) : IAddingStatusEffectPolicy
+{
+    public void OnAdd(
+        Character character, 
+        Character author, 
+        StatusEffect newStatusEffect,
+        IReadOnlyList<StatusEffect> currentStatusEffects,
+        IReadOnlyDictionary<string, List<StatusEffect>> currentStatusEffectsById,
+        IReadOnlyDictionary<string, List<StatusEffect>> currentStatusEffectsByTag,
+        Action<StatusEffect, Character> addStatusEffectFunc, 
+        Action<StatusEffect> removeStatusEffectFunc)
+    {
+        
+        if (currentStatusEffectsById.GetValueOrDefault(newStatusEffect.Id, []).Count < limit)
+        {
+            addStatusEffectFunc(newStatusEffect, author);
+        }
+    }
+}
