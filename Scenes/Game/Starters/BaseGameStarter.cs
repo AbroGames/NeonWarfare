1b05598a5cd9c7e0b60021312e754c1564a50295
Bronuh <bronyx@yandex.ru>
2026-03-22 17:33:46 +0300

    Expose default host and port

--- Scenes/Game/Starters/BaseGameStarter.cs
+++ Scenes/Game/Starters/BaseGameStarter.cs
@@ -3,8 +3,8 @@
 public abstract class BaseGameStarter
 {
     protected const string Localhost = "127.0.0.1";
-    protected const string DefaultHost = Localhost;
-    protected const int DefaultPort = 25566;
+    public const string DefaultHost = Localhost;
+    public const int DefaultPort = 25566;
 
     public virtual void Init(Game game)
     {
