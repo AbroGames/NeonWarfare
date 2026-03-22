a1bcf850761ab36e7bfac9d0f495c456fdea507e
Bronuh <bronyx@yandex.ru>
2026-03-22 17:36:11 +0300

    Prepare for last used game type preservation

--- Scenes/NeonTemp/UI/Menu/SettingsSystem/GameSettings.cs
+++ Scenes/NeonTemp/UI/Menu/SettingsSystem/GameSettings.cs
@@ -1,7 +1,15 @@
 ﻿using Godot;
+using NeonWarfare.Scenes.Game.Starters;
 
 namespace Kludgeful.Main.SettingsSystem;
 
+public enum ResumableGame
+{
+    None,
+    RunSingleplayer,
+    CreateServer,
+    ConnectToServer,
+}
 public partial class GameSettings : GameSettingsBase
 {
     [Hint("Name of the player in the multiplayer")]
@@ -10,5 +18,11 @@ public partial class GameSettings : GameSettingsBase
     [Hint("Color of the player character")]
     public Color PlayerColor { get; set; } = Colors.GreenYellow;
 
-    [Hide] public string LastConnectedHost { get; set; } = "127.0.0.1";
+    [Hide] public ResumableGame FastResumeAvailable { get; set; } = ResumableGame.None;
+    
+    [Hide] public string LastConnectedHost { get; set; } = BaseGameStarter.DefaultHost;
+    [Hide] public int LastConnectedPort { get; set; } = BaseGameStarter.DefaultPort;
+    
+    [Hide] public int LastHostedPort { get; set; } = BaseGameStarter.DefaultPort;
+    [Hide] public bool LastHostedIsDedicated { get; set; } = false;
 }
