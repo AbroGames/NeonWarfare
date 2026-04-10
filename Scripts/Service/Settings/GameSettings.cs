78fd11a2cb9c3f93a56abc739ef7d81df0e4c443
Evgeniy Serov <kejik148@gmail.com>
2026-04-10 13:33:21 +0500

    Add PlayerUid to GameSettings
    (cherry picked from commit 664856998a777cfb798bf9e8a346b0275bc9964f)

--- Scripts/Service/Settings/GameSettings.cs
+++ Scripts/Service/Settings/GameSettings.cs
@@ -3,6 +3,7 @@
 namespace NeonWarfare.Scripts.Service.Settings;
 
 public record GameSettings(
+    string PlayerUid,
     string PlayerNick,
     Color PlayerColor,
     string Locale,
@@ -12,6 +13,7 @@ public record GameSettings(
     public static GameSettings GetDefault()
     {
         return new(
+            PlayerUid: new UidGenerator().Generate(),
             PlayerNick: "Player",
             PlayerColor: new Color(1, 1, 1),
             Locale: Services.I18N.GetUserOsLocaleInfoOrDefault().Code,
---
+++ Scripts/Service/Settings/UidGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace GodotTemplate.Scripts.Service.Settings;
+
+public class UidGenerator
+{
+    private const string DefaultAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private string _alphabet;
+
+    public UidGenerator(string alphabet = DefaultAlphabet)
+    {
+        _alphabet = alphabet;
+    }
+
+    public string Generate()
+    {
+        return RandomNumberGenerator.GetString(_alphabet, 10) +
+               "-" + 
+               RandomNumberGenerator.GetString(_alphabet, 10);
+    }
+}
