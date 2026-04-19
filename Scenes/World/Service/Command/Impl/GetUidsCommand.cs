9b4c0680d22a04d2afa78574224fc5a2d91f90d3
Evgeniy Serov <kejik148@gmail.com>
2026-04-19 19:05:21 +0500

    Command for get uids

---
+++ Scenes/World/Service/Command/Impl/GetUidsCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Humanizer;
+
+namespace NeonWarfare.Scenes.World.Service.Command.Impl;
+
+public class GetUidsCommand : ICommandProcessor
+{
+    private const string PlayersMessage = "Players online: {0}.\nPlayers offline: {1}.";
+    private const string NotFoundText = "not found";
+    
+    public string GetCommand() => "uids";
+    public string GetDescription() => "Show list of players with UIDs.";
+    public bool IsRequiringAdmin() => false;
+
+    public void ProcessCommand(int senderId, string command, World world)
+    {
+        string playersOnline = string.Join(", ", world.FacadeService.GetOnlinePlayers()
+            .Select(playerData => $"{playerData.Nick} (uid: {playerData.Uid})")
+            .DefaultIfEmpty(NotFoundText));
+        
+        string playersOffline = string.Join(", ", world.FacadeService.GetOfflinePlayers()
+            .Select(playerData => $"{playerData.Nick} (uid: {playerData.Uid})")
+            .DefaultIfEmpty(NotFoundText));
+        
+        string message = PlayersMessage.FormatWith(playersOnline, playersOffline);
+        world.ChatService.TrySendNewMessage(message, senderId);
+    }
+}
